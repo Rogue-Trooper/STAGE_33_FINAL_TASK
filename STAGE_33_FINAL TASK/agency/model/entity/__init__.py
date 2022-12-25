2d840a39a9a78c4dec772e06e74d1cf2fd17436d
Rogue-Trooper <k413502@yahoo.com>
2022-12-25 13:51:55 +0300

    final update

--- STAGE_33_FINAL TASK/agency/model/entity/__init__.py
+++
@@ -1,6 +0,0 @@
-from agency.model.entity.tour import Tour
-from agency.model.entity.fare import Fare
-from agency.model.entity.hotel import Hotel
-from agency.model.entity.insurance import Insurance
-from agency.model.entity.offer import Offer
-from agency.model.entity.giver import Giver
--- STAGE_33_FINAL TASK/agency/view/console.py
+++
@@ -1,4 +0,0 @@
-class Console:
-    @staticmethod
-    def output(msg):
-        print(msg)
